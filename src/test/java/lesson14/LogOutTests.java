3c509958c5a04e695231bb013802207402f6c9f1
AnnaBudinova <anbori@abv.bg>
2023-07-24 09:39:37 +0300

    Update lesson14: - rename java class; - add wait for test "testLogOutFromNewPost" - add new Data provider - add new check in test "testLogOutFromHome"

--- src/test/java/lesson14/LogOutTests.java
+++ src/test/java/lesson14/LogOutTests.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,7 +13,7 @@ import org.testng.annotations.*;
 
 import java.time.Duration;
 
-public class LogOut {
+public class LogOutTests {
     private WebDriver driver;
     @BeforeSuite
     protected final void setupTestSuite() {
@@ -39,8 +38,15 @@ public class LogOut {
     }
     @DataProvider(name = "getUsers")
     public Object[][] getUsers() {
-        return new Object[][]{ //login with username
+        return new Object[][]{
                 {"anbori@abv.bg", "_Passw0rd", "annabudinova"},
+        };
+    }
+
+    @DataProvider(name = "loginUser")
+    public Object[][] loginUsers() {
+        return new Object[][]{
+                {"anbori@abv.bg", "_Passw0rd"},
         };
     }
 
@@ -124,8 +130,8 @@ public class LogOut {
         wait.until(ExpectedConditions.visibilityOf(signInElement));
     }
 
-    @Test(dataProvider = "getUsers")
-    public void testLogOutFromNewPost(String user, String password, String name){
+    @Test(dataProvider = "loginUser")
+    public void testLogOutFromNewPost(String user, String password){
         driver.get("http://training.skillo-bg.com:4300/posts/all");
         WebElement loginLink = driver.findElement(By.id("nav-link-login"));
         loginLink.click();
@@ -145,7 +151,8 @@ public class LogOut {
         WebElement signInButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("sign-in-button")));
         signInButton.click();
 
-        WebElement newPostLink = wait.until(ExpectedConditions.elementToBeClickable(By.id("nav-link-new-post")));
+        WebDriverWait postLinkWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        WebElement newPostLink = postLinkWait.until(ExpectedConditions.elementToBeClickable(By.id("nav-link-new-post")));
         newPostLink.click();
 
         wait.until(ExpectedConditions.urlContains("http://training.skillo-bg.com:4300/posts/create"));
@@ -162,8 +169,8 @@ public class LogOut {
         wait.until(ExpectedConditions.visibilityOf(signInElement));
     }
 
-    @Test(dataProvider = "getUsers")
-    public void testLogOutFromHome(String user, String password, String name){
+    @Test(dataProvider = "loginUser")
+    public void testLogOutFromHome(String user, String password){
         driver.get("http://training.skillo-bg.com:4300/posts/all");
         WebElement loginLink = driver.findElement(By.id("nav-link-login"));
         loginLink.click();
@@ -187,6 +194,9 @@ public class LogOut {
 
         WebElement isTextDisplayed = driver.findElement(By.id("nav-link-home"));
         wait.until(ExpectedConditions.visibilityOf(isTextDisplayed));
+
+        WebElement isBoxDisplayed = driver.findElement(By.xpath("//*[@class='col-12 offset-md-3 col-md-6 all-users-container']"));
+        wait.until(ExpectedConditions.visibilityOf(isBoxDisplayed));
 
         WebElement logoutLink = driver.findElement(By.xpath("//*[@class='fas fa-sign-out-alt fa-lg']"));
         logoutLink.click();
